7cafd3187fb711bd5c09a3a8f372cf7e8452218b
Madeleine Filloux <mfilloux@gmail.com>
2018-07-02 21:23:39 -0500

    basic models and react-router-dom routing

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const db = require("./models");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
